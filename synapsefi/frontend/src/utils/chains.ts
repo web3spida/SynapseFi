bca37a5aefb937932e48139d15e55cad6d48f2d4
Joseph Emmanuel <117820245+Fmsticks2@users.noreply.github.com>
2025-10-17 20:46:38 +0100

    envv variables setup

--- synapsefi/frontend/src/utils/chains.ts
+++ synapsefi/frontend/src/utils/chains.ts
@@ -3,7 +3,6 @@ import type { Chain } from 'viem'
 export const polygonAmoy: Chain = {
   id: 80002,
   name: 'Polygon Amoy',
-  network: 'polygon-amoy',
   nativeCurrency: { name: 'Polygon', symbol: 'POL', decimals: 18 },
   rpcUrls: {
     default: { http: ['https://rpc-amoy.polygon.technology'] },
